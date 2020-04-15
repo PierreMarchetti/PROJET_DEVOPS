949942b4db8e9e34d408e549dcd72f08d673d7bd
Pierre Marchetti <pierre.marh@gmail.com>
2020-04-15 10:29:29 +0200

    Création du squelette de dataframe

---
+++ src/projetdevops/Dataframe.java
@@ -0,0 +1,53 @@
+package projetdevops;
+
+public class Dataframe {
+	
+	public Dataframe(String filepath) {
+		
+	}
+	
+	public Dataframe(Object[][] table) {
+		
+	}
+	
+	public String displayDataframe() {
+		return null;
+		
+	}
+	
+	public String head() {
+		return null;
+		
+	}
+	
+	
+	public String tail() {
+		return null;
+		
+	}
+	
+	public Dataframe iloc(int index) {
+		return null;
+	}
+	
+	public Dataframe loc(String label) {
+		return null;
+	}
+	
+	public float sum() {
+		return 0;
+	}
+	
+	public float mean() {
+		return 0;
+	}
+	
+	public float max() {
+		return 0;
+	}
+	
+	public float min() {
+		return 0;
+	}
+	
+}
---
+++ src/projetdevops/DataframeTest.java
@@ -0,0 +1,69 @@
+package projetdevops;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DataframeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testDataframeString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDataframeObjectArrayArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayDataframe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHead() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIloc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMax() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMin() {
+		fail("Not yet implemented");
+	}
+
+}
