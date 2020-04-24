5e4fb91835bb60caa4bc20dfba19f9f01351175e
Pierre Marchetti <pierre.marh@gmail.com>
2020-04-24 22:26:48 +0200

    correction de la classe DataframeTest et complétion de la classe dataframe

--- src/main/java/Dataframe.java
+++ src/main/java/Dataframe.java
@@ -1,53 +1,247 @@
 package main.java;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
 public class Dataframe {
-	
-	public Dataframe(String filepath) {
+
+	private ArrayList<ArrayList<Object>> dataframeBody;
+	private ArrayList<Class<?>> columntypes;
+
+
+	public Dataframe(String filepath) throws Exception {
+		dataframeBody = new ArrayList<ArrayList<Object>>();
+		columntypes = new ArrayList<Class<?>>();
+
+		String line = "";
+		try {
+
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
+
+			line = br.readLine();
+			String[] classes = line.split(",");
+			for (String sClass : classes) {
+				columntypes.add(Class.forName(sClass));
+				ArrayList<Object> column = new ArrayList<Object>();
+				dataframeBody.add(column);
+
+			}
+			while ((line = br.readLine()) != null) {
+				String[] items = line.split(",");
+
+				for (int i = 0; i < items.length; i++) {
+
+					if (items[i].equals("")) {
+						dataframeBody.get(i).add(null);
+					} else {
+						try {
+							Constructor<?> ctor = columntypes.get(i).getConstructor(String.class);
+							Object o = ctor.newInstance(items[i]);
+
+							dataframeBody.get(i).add(o);
+						} catch (NoSuchMethodException | SecurityException | InstantiationException
+								| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+							throw new Exception();
+						}
+
+					}
+
+				}
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		
 	}
-	
-	public Dataframe(Object[][] table) {
-		
+
+	public Dataframe(Object[][] table) throws Exception {
+		dataframeBody = new ArrayList<ArrayList<Object>>();
+		columntypes = new ArrayList<Class<?>>();
+
+		for (Object o : table[0]) {
+			ArrayList<Object> column = new ArrayList<Object>();
+			column.add(o);
+			dataframeBody.add(column);
+			columntypes.add(o.getClass());
+		}
+
+		for (int i = 1; i < table.length; i++) {
+
+			for (int j = 0; j < table[i].length; j++) {
+				if (table[i][j] != null) {
+					if (table[i][j].getClass() != columntypes.get(j)) {
+
+						throw new Exception("les éléments d'un label n'ont pas tous le même type");
+					}
+				}
+
+				dataframeBody.get(j).add(table[i][j]);
+			}
+		}
 	}
-	
+
 	public String displayDataframe() {
-		return null;
-		
+		String result = "  ";
+
+		for (int i = 1; i < dataframeBody.size() + 1; i++) {
+			result += i + " ";
+		}
+		result += "\n";
+
+		for (int i = 0; i < dataframeBody.get(0).size(); i++) {
+			result += (char) (i + 97);
+			result += " ";
+			for (int j = 0; j < dataframeBody.size(); j++) {
+				if (dataframeBody.get(j).get(i) == null)
+					result += "NaN ";
+				else
+					result += dataframeBody.get(j).get(i) + " ";
+			}
+			result += "\n";
+		}
+
+		return result;
+
 	}
-	
+
 	public String head() {
-		return null;
-		
+		String result = "  ";
+
+		for (int i = 1; i < dataframeBody.size() + 1; i++) {
+			result += i + " ";
+		}
+		result += "\n";
+
+		for (int i = 0; i < Math.min(dataframeBody.get(0).size(), 2); i++) {
+			result += (char) (i + 97);
+			result += " ";
+			for (int j = 0; j < dataframeBody.size(); j++) {
+				if (dataframeBody.get(j).get(i) == null)
+					result += "NaN ";
+				else
+					result += dataframeBody.get(j).get(i) + " ";
+			}
+			result += "\n";
+		}
+
+		return result;
 	}
-	
-	
+
 	public String tail() {
-		return null;
-		
+		String result = "  ";
+
+		for (int i = 1; i < dataframeBody.size() + 1; i++) {
+			result += i + " ";
+		}
+		result += "\n";
+
+		for (int i = (dataframeBody.size() < 2 ? 0 : dataframeBody.size() - 2); i < dataframeBody.get(0).size(); i++) {
+			result += (char) (i + 97);
+			result += " ";
+			for (int j = 0; j < dataframeBody.size(); j++) {
+				if (dataframeBody.get(j).get(i) == null)
+					result += "NaN ";
+				else
+					result += dataframeBody.get(j).get(i) + " ";
+			}
+			result += "\n";
+		}
+
+		return result;
+
 	}
-	
-	public Dataframe iloc(int index) {
-		return null;
+
+	public Dataframe iloc(int[] indexes) throws Exception {
+		Object[][] table = new Object[indexes.length][dataframeBody.get(0).size()];
+
+		for (int i = 0; i < indexes.length; i++) {
+			for (int j = 0; j < dataframeBody.size(); j++) {
+
+				table[i][j] = dataframeBody.get(j).get(indexes[i]);
+			}
+		}
+		return new Dataframe(table);
 	}
-	
-	public Dataframe loc(String label) {
-		return null;
+
+	public Dataframe loc(int[] labels) throws Exception {
+		Object[][] table = new Object[dataframeBody.size()][labels.length];
+
+		for (int i = 0; i < labels.length; i++) {
+			for (int j = 0; j < dataframeBody.size(); j++) {
+
+				table[j][i] = dataframeBody.get(labels[i]).get(j);
+			}
+		}
+		return new Dataframe(table);
 	}
-	
-	public float sum() {
-		return 0;
+
+	public BigDecimal sum(int label) {
+
+		BigDecimal r = new BigDecimal(0);
+
+		if (!Number.class.isAssignableFrom(columntypes.get(label))) {
+			return r;
+		}
+
+		for (Object object : dataframeBody.get(label)) {
+			if (object != null) {
+				r = r.add(new BigDecimal(object.toString()));
+			}
+		}
+		return r;
 	}
-	
-	public float mean() {
-		return 0;
+
+	public BigDecimal mean(int label) {
+
+		if (!Number.class.isAssignableFrom(columntypes.get(label))) {
+			return new BigDecimal(0);
+		}
+
+		return sum(label).divide(new BigDecimal(dataframeBody.get(0).size()), 10, RoundingMode.HALF_UP);
 	}
-	
-	public float max() {
-		return 0;
+
+	public BigDecimal max(int label) {
+
+		BigDecimal r = new BigDecimal(0);
+
+		if (!Number.class.isAssignableFrom(columntypes.get(label))) {
+			return r;
+		}
+
+		for (Object object : dataframeBody.get(label)) {
+			if (object != null) {
+				r = r.max(new BigDecimal(object.toString()));
+			}
+		}
+
+		return r;
 	}
-	
-	public float min() {
-		return 0;
+
+	public BigDecimal min(int label) {
+		BigDecimal r = new BigDecimal(0);
+
+		if (!Number.class.isAssignableFrom(columntypes.get(label)) || dataframeBody.get(label).size() == 0) {
+			return r;
+		}
+
+		r = new BigDecimal(dataframeBody.get(label).get(0).toString());
+
+		for (Object object : dataframeBody.get(label)) {
+			if (object != null) {
+				r = r.min(new BigDecimal(object.toString()));
+			}
+		}
+
+		return r;
 	}
-	
+
+	public ArrayList<ArrayList<Object>> getDataframeBody() {
+		return dataframeBody;
+	}
 }
--- src/test/java/DataframeTest.java
+++ src/test/java/DataframeTest.java
@@ -2,6 +2,8 @@ package test.java;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 
 import org.junit.BeforeClass;
@@ -10,53 +12,14 @@ import org.junit.Test;
 import main.java.Dataframe;
 
 public class DataframeTest {
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
 	@Test
-	public void testDataframeFile() {
-		 String filepath = "src/test/resources/testfile.csv";
-		 
-		 Dataframe dataframe = new Dataframe(filepath);
-		 
-		 ArrayList<ArrayList<?>> dataframeBody = new ArrayList<ArrayList<?>>();
-			
-		ArrayList<Integer> column1 = new ArrayList<Integer>();
-		column1.add(1);
-		column1.add(null);
-		column1.add(3);
+	public void testDataframeFile() throws Exception {
+		String filepath = "src/test/resources/testfile.csv";
 
-		ArrayList<Double> column2 = new ArrayList<Double>();
-		column2.add(2.0);
-		column2.add(3.6);
-		column2.add(5.9);
-		
-		ArrayList<String> column3 = new ArrayList<String>();
-		column3.add("aa");
-		column3.add("bb");
-		column3.add("cc");
-		
-		dataframeBody.add(column1);
-		dataframeBody.add(column2);
-		dataframeBody.add(column3);
-		
-		assertEquals(dataframeBody, dataframe.getDataframeBody());
-	}
+		Dataframe dataframe = new Dataframe(filepath);
 
-	@Test(expected = Exception.class)
-	public void testDataframeFileTypeError() {
-		 String filepath = "src/test/resources/testfileError.csv";
-		 Dataframe dataframe = new Dataframe(filepath);
-	}
-	
-	@Test
-	public void testDataframeTable() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
 		ArrayList<ArrayList<?>> dataframeBody = new ArrayList<ArrayList<?>>();
-		
+
 		ArrayList<Integer> column1 = new ArrayList<Integer>();
 		column1.add(1);
 		column1.add(null);
@@ -66,158 +29,185 @@ public class DataframeTest {
 		column2.add(2.0);
 		column2.add(3.6);
 		column2.add(5.9);
-		
+
 		ArrayList<String> column3 = new ArrayList<String>();
 		column3.add("aa");
 		column3.add("bb");
 		column3.add("cc");
-		
+
 		dataframeBody.add(column1);
 		dataframeBody.add(column2);
 		dataframeBody.add(column3);
-		
+
 		assertEquals(dataframeBody, dataframe.getDataframeBody());
-		
-//		ArrayList<String> indexes = new ArrayList<String>();
-//		indexes.add("1");
-//		indexes.add("2");
-//		indexes.add("3");
-//		assertEquals(indexes, dataframe.getIndexList());
-//		
-//		ArrayList<String> labels = new ArrayList<String>();
-//		indexes.add("a");
-//		indexes.add("b");
-//		indexes.add("c");
-//		assertEquals(labels, dataframe.getLabelList());		
 	}
-	
+
 	@Test(expected = Exception.class)
-	public void testDataframeTableTypeError() {
-		Object[][] table = {{1},{"this string is not an int"}};
-		Dataframe dataframe =  new Dataframe(table);
+	public void testDataframeFileTypeError() throws Exception {
+		String filepath = "src/test/resources/testfileError.csv";
+
+		Dataframe dataframe = new Dataframe(filepath);
 	}
 
 	@Test
-	public void testDisplayDataframe() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		String expected = "  1 2 3\na 1 2.0 aa\nb NaN 3.6 bb\n3 5.9 cc";
+	public void testDataframeTable() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		ArrayList<ArrayList<Object>> dataframeBody = new ArrayList<ArrayList<Object>>();
+
+		ArrayList<Object> column1 = new ArrayList<Object>();
+		column1.add(1);
+		column1.add(null);
+		column1.add(3);
+
+		ArrayList<Object> column2 = new ArrayList<Object>();
+		column2.add(2.0);
+		column2.add(3.6);
+		column2.add(5.9);
+
+		ArrayList<Object> column3 = new ArrayList<Object>();
+		column3.add("aa");
+		column3.add("bb");
+		column3.add("cc");
+
+		dataframeBody.add(column1);
+		dataframeBody.add(column2);
+		dataframeBody.add(column3);
+
+		assertEquals(dataframeBody, dataframe.getDataframeBody());
+	}
+
+	@Test(expected = Exception.class)
+	public void testDataframeTableTypeError() throws Exception {
+		Object[][] table = { { 1 }, { "this string is not an int" } };
+		Dataframe dataframe = new Dataframe(table);
+	}
+
+	@Test
+	public void testDisplayDataframe() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		String expected = "  1 2 3 \na 1 2.0 aa \nb NaN 3.6 bb \nc 3 5.9 cc \n";
 		String actual = dataframe.displayDataframe();
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testHead() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		String expected = "  1 2 3\na 1 2.0 aa\nb NaN 3.6 bb";
+	public void testHead() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		String expected = "  1 2 3 \na 1 2.0 aa \nb NaN 3.6 bb \n";
 		String actual = dataframe.head();
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testTail() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		String expected = "  1 2 3\nb NaN 3.6 bb\n3 5.9 cc";
+	public void testTail() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		String expected = "  1 2 3 \nb NaN 3.6 bb \nc 3 5.9 cc \n";
 		String actual = dataframe.tail();
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testIloc() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		int[] indexes = {0,1};
+	public void testIloc() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+		int[] indexes = { 0, 2 };
 		Dataframe actual = dataframe.iloc(indexes);
-		
-		Object[][] tableExpected = {{1,2.0,"aa"},{3,5.9,"cc"}};
 
-		Dataframe expected =  new Dataframe(tableExpected);
-		
-		assertEquals(expected, actual);
+		Object[][] tableExpected = { { 1, 2.0, "aa" }, { 3, 5.9, "cc" } };
+
+		Dataframe expected = new Dataframe(tableExpected);
+
+		assertEquals(expected.getDataframeBody(), actual.getDataframeBody());
 
 	}
 
 	@Test
-	public void testLoc() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		String[] labels = {"a","b"};
+	public void testLoc() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+		int[] labels = { 0, 1 };
 		Dataframe actual = dataframe.loc(labels);
-		
-		Object[][] tableExpected = {{1,2.0},{null,3.6},{3,5.9}};
 
-		Dataframe expected =  new Dataframe(tableExpected);
-		
-		assertEquals(expected, actual);
-	}
+		Object[][] tableExpected = { { 1, 2.0 }, { null, 3.6 }, { 3, 5.9 } };
 
-	@Test
-	public void testSum() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(4,dataframe.sum(0));
-	}
-	
-	public void testSumNonNumber() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(0,dataframe.sum(2));
+		Dataframe expected = new Dataframe(tableExpected);
+
+		assertEquals(expected.getDataframeBody(), actual.getDataframeBody());
 	}
 
 	@Test
-	public void testMean() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(11.5/3,dataframe.mean(1));
-	}
-	
-	@Test
-	public void testMeanNonNumber() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(0,dataframe.mean(2));
+	public void testSum() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(4).compareTo(dataframe.sum(0)) == 0);
 	}
 
 	@Test
-	public void testMax() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(3,dataframe.max(0));
+	public void testSumNonNumber() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(0).compareTo(dataframe.sum(2)) == 0);
 	}
-	
-	@Test
-	public void testMaxNonNumber() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(0,dataframe.max(2));
-	}
-	
 
 	@Test
-	public void testMin() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(3,dataframe.min(1));
+	public void testMean() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(11.5).divide(new BigDecimal(3), 10, RoundingMode.HALF_UP)
+				.compareTo(dataframe.mean(1)) == 0);
 	}
-	
+
 	@Test
-	public void testMinNonNumber() {
-		Object[][] table = {{1,2.0,"aa"},{null,3.6,"bb"},{3,5.9,"cc"}};
-		Dataframe dataframe =  new Dataframe(table);
-		
-		assertEquals(0,dataframe.min(2));
+	public void testMeanNonNumber() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(0).compareTo(dataframe.mean(2)) == 0);
+	}
+
+	@Test
+	public void testMax() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(3).compareTo(dataframe.max(0)) == 0);
+	}
+
+	@Test
+	public void testMaxNonNumber() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(0).compareTo(dataframe.max(2)) == 0);
+
+	}
+
+	@Test
+	public void testMin() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(2.0).compareTo(dataframe.min(1)) == 0);
+	}
+
+	@Test
+	public void testMinNonNumber() throws Exception {
+		Object[][] table = { { 1, 2.0, "aa" }, { null, 3.6, "bb" }, { 3, 5.9, "cc" } };
+		Dataframe dataframe = new Dataframe(table);
+
+		assert (new BigDecimal(0).compareTo(dataframe.min(2)) == 0);
+
 	}
 
 }
